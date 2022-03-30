4ce7baea8c19518f0460e18bf0396cc4f291b6f5
Oscar Isberg <oscar.isberg@iths.se>
2022-03-30 13:31:42 +0200

    First commit

--- HomeFinder/Controllers/AdminOptions.cs
+++ HomeFinder/Controllers/AdminOptions.cs
@@ -1,12 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HomeFinder.Models;
+using HomeFinder.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace HomeFinder.Controllers
 {
     public class AdminOptions : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        public AdminOptions(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ListUsers()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            if (!_signInManager.IsSignedIn(User))
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
+        //Samma metod som i UserController för att skapa användare
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid) // Om alla properties validerar med sina attributes:
+            {
+                var user = new ApplicationUser()
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded) // Om det gick bra att skapa användaren:
+                {
+                    if(_signInManager.IsSignedIn(User) && User.IsInRole("Admin"))
+                    {
+                        return RedirectToAction("ListUsers", "AdminOptions");
+                    }
+                    await _signInManager.SignInAsync(user, model.RememberMe);
+                    return RedirectToAction("Index", "Home");
+                }
+                else // Om det inte gick att skapa användaren:
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View(model);
         }
     }
 }
