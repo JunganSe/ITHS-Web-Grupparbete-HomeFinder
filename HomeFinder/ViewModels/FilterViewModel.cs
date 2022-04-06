19f92db24fe17fa84e6222a48d3f18fdb3d664e6
Dennis Stegnell <dennis.stegnell@iths.se>
2022-04-06 14:51:02 +0200

    Added filter page

---
+++ HomeFinder/ViewModels/FilterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using HomeFinder.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeFinder.ViewModels
+{
+    public class FilterViewModel
+    {
+        public int MinPrice { get; set; }
+        public int MaxPrice { get; set; }
+        public int MinArea { get; set; }
+        public int MaxArea { get; set; }
+        public int MinNumberOfRooms { get; set; }
+        public int MaxNumberOfRooms { get; set; }
+        [Display(Name = "Property Type")]
+        public List<PropertyType> PropertyTypes { get; set; }
+        public List<Property> Properties { get; set; }
+        public List<Tenure> Tenures { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string PostalCode { get; set; }
+
+
+    }
+}
