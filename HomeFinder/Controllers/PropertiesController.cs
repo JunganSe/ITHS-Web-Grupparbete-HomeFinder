bdfe2e81d2569a1e2dffe5090a5492905147c34b
SetragGodoshian <setrag.godoshian@iths.se>
2022-04-07 13:49:34 +0200

    gjort partial för att vissa properties

--- HomeFinder/Controllers/PropertiesController.cs
+++ HomeFinder/Controllers/PropertiesController.cs
@@ -38,6 +38,7 @@ namespace HomeFinder.Controllers
             return View(await homeFinderContext.ToListAsync());
         }
 
+        [AllowAnonymous]
         // GET: Properties/Details/5
         public async Task<IActionResult> Details(int? id)
         {
