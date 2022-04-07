f262a6798086166e8010015c729890f65ca96465
elsafelicia <91050613+elsafelicia@users.noreply.github.com>
2022-04-07 15:40:41 +0200

    Added Google oAuth

--- HomeFinder/Controllers/AccountController.cs
+++ HomeFinder/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using HomeFinder.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace HomeFinder.Controllers
@@ -68,15 +70,91 @@ namespace HomeFinder.Controllers
 
         // GET: /User/Login
         [HttpGet]
-        public IActionResult Login()
+        public async Task<IActionResult> Login(string returnUrl)
         {
             if (!_signInManager.IsSignedIn(User))
             {
-                return View();
+                LoginViewModel model = new LoginViewModel
+                {
+                    ReturnUrl = returnUrl,
+                    ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+                };
+                return View(model);
             }
             else
             {
                 return RedirectToAction("Index", "Home");
+            }
+        }
+        // 
+        [HttpPost]
+        public IActionResult ExternalLogin(string provider, string returnUrl)
+        {
+            var redirectUrl = Url.Action("ExternalLoginCallback", "Account",
+                                new { ReturnUrl = returnUrl });
+            var properties = _signInManager
+                .ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            return new ChallengeResult(provider, properties);
+        }
+
+        public async Task<IActionResult>
+            ExternalLoginCallback(string returnUrl = null, string remoteError = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            LoginViewModel loginViewModel = new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+                ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList()
+            };
+            if (remoteError != null)
+            {
+                ModelState.AddModelError(string.Empty, $"Error from external provider: { remoteError}");
+                return View("Login", loginViewModel);
+            }
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+            if (info == null)
+            {
+                ModelState.AddModelError(string.Empty, "Error loading external login information.");
+                return View("Login", loginViewModel);
+            }
+            var signInResult = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, 
+                                                                                        info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            if (signInResult.Succeeded)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+
+                if (email != null)
+                {
+                    var user = await _userManager.FindByEmailAsync(email);
+
+                    if (user == null)
+                    {
+                        user = new ApplicationUser
+                        {
+                            UserName = info.Principal.FindFirstValue(ClaimTypes.Email),
+                            Email = info.Principal.FindFirstValue(ClaimTypes.Email),
+                            FirstName = info.Principal.FindFirstValue(ClaimTypes.Name),
+                            LastName = info.Principal.FindFirstValue(ClaimTypes.Surname)
+                        };
+
+                        await _userManager.CreateAsync(user);
+                    }
+
+                    await _userManager.AddLoginAsync(user, info);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+
+                    return LocalRedirect(returnUrl);
+                }
+                //TODO: ViewData? Lägg till mail för support
+                ViewBag.ErrorTitle = $"Email claim not recived from: {info.LoginProvider}";
+                ViewBag.ErrorMessage = "Please contact support at Goat@Goats.bah";
+
+                return View("Error");
+                
             }
         }
 
