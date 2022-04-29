e58b3d50481cef2d01d7393f59d03a6f68724463
Johan Ljungberg <jlj.9@hotmail.com>
2022-04-29 08:53:34 +0200

    Home/Index använder nu ListPropertiesPartial

--- HomeFinder/ViewModels/HomeViewModel.cs
+++ HomeFinder/ViewModels/HomeViewModel.cs
@@ -8,6 +8,8 @@ namespace HomeFinder.ViewModels
         public List<Property> Top3Viewed { get; set; }
         public List<Property> Top3Size { get; set; }
         public List<Property> Top3Price { get; set; }
+
+        public List<Property> Properties { get; set; } // Tilldelas i View för att användas i _ListPropertiesPartial
         public List<Image> Images { get; set; }
     }
 }
